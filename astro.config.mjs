6500c9a9ce1623d9954b498c14215a2e2832ee39
Francis Castillo <franciscastillocruz@proton.me>
2024-09-16 00:09:38 -0400

    Iconos sin utilizar retirados y astro shield removido

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import { resolve } from "node:path";
-
 import tailwind from "@astrojs/tailwind";
-import { shield } from "@kindspells/astro-shield";
-
-const rootDir = new URL(".", import.meta.url).pathname;
-const modulePath = resolve(rootDir, "src", "generated", "sriHashes.mjs");
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    shield({
-      sri: {
-        hashesModule: modulePath,
-      },
-    }),
-  ],
+  integrations: [tailwind()],
 });
